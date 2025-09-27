cd3d9213c79409929621c3db6594c779d21f3f46
Jcmedinah <jean.medina0399@gmail.com>
2025-09-27 11:57:50 -0500

    Fase 4: Menú interactivo de aventura implementado"

--- 1232598910-Jean-Carlo-Medina-Delgado/app.js
+++ 1232598910-Jean-Carlo-Medina-Delgado/app.js
@@ -2,16 +2,16 @@ const nombreHeroe = prompt('Ingrese el nombre del heroe: ');
 let puntosSalud = 100;
 let puntosExperiencia = 0;
 
-function mostrarEstadisticas(){
+function mostrarEstadisticas() {
     console.log(`Heroe: [${nombreHeroe}], Salud: ${puntosSalud} HP, Experiencia: ${puntosExperiencia} XP`);
 }
 
 
 
-const recibirDanio = function(cantidadDanio){
+const recibirDanio = function (cantidadDanio) {
     puntosSalud = puntosSalud - cantidadDanio;
 
-    if(puntosSalud < 0){
+    if (puntosSalud < 0) {
         puntosSalud = 0;
     }
 
@@ -22,7 +22,7 @@ const recibirDanio = function(cantidadDanio){
     }
 }
 
-const ganarExperiencia = (cantidadXP) =>{
+const ganarExperiencia = (cantidadXP) => {
     puntosExperiencia = puntosExperiencia + cantidadXP;
     console.log(`Ganaste [${cantidadXP}] XP. Experiencia total: [${puntosExperiencia}] XP.`);
 }
@@ -37,7 +37,7 @@ const simularBatalla = () => {
     ganarExperiencia(50);
 }
 
-function diagnosticoHeroe(){
+function diagnosticoHeroe() {
     if (puntosSalud > 70) {
         console.log("Tu salud es excelente");
     } else if (puntosSalud > 30) {
@@ -57,5 +57,45 @@ function diagnosticoHeroe(){
     }
 }
 
-simularBatalla();
-diagnosticoHeroe();
+function iniciarAventura() {
+    while (true) {
+        const accion = prompt(`
+        Menú de Aventura:
+        1 - Luchar contra un monstruo
+        2 - Tomar poción de salud
+        3 - Completar misión (ganar XP)
+        4 - Ver diagnóstico del Héroe
+        5 - Mostrar Estadísticas
+        6 - Retirarse de la aventura
+        `);
+    
+        switch (accion) {
+            case '1':
+                simularBatalla();
+                break;
+            case '2':
+                let cantidadPocion = parseInt(prompt("Ingrese la cantidad de salud que restaura la poción: "));
+                if (isNaN(cantidadPocion) || cantidadPocion < 0) {
+                    console.log("Cantidad inválida. La poción no tiene efecto.");
+                }
+                puntosSalud += cantidadPocion;
+                break;
+            case '3':
+                ganarExperiencia(20);
+                break;
+            case '4':
+                diagnosticoHeroe();
+                break; 
+            case '5':
+                mostrarEstadisticas();
+                break;
+            case '6':
+                console.log("Has decidido retirarte de la aventura. ¡Hasta la próxima!");
+                return;
+            default:
+                console.log("Opción no válida");
+        }
+    }
+}
+
+iniciarAventura();
