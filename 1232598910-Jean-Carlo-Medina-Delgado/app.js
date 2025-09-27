24a1e9132ec07886e6a08faa1dfd8a6cd0fba997
Jcmedinah <jean.medina0399@gmail.com>
2025-09-27 11:34:36 -0500

    Fase 2: Implementadas funciones para recibir daño y ganar experiencia

--- 1232598910-Jean-Carlo-Medina-Delgado/app.js
+++ 1232598910-Jean-Carlo-Medina-Delgado/app.js
@@ -6,4 +6,30 @@ function mostrarEstadisticas(){
     console.log(`Heroe: [${nombreHeroe}], Salud: ${puntosSalud} HP, Experiencia: ${puntosExperiencia} XP`);
 }
 
+
+
+const recibirDanio = function(cantidadDanio){
+    puntosSalud = puntosSalud - cantidadDanio;
+
+    if(puntosSalud < 0){
+        puntosSalud = 0;
+    }
+
+    if (puntosSalud === 0) {
+        console.log(`¡Has sido derrotado!`);
+    } else {
+        console.log(`Recibiste [${cantidadDanio}] de daño. Salud restante: [${puntosSalud}] HP.`);
+    }
+}
+
+const ganarExperiencia = (cantidadXP) =>{
+    puntosExperiencia = puntosExperiencia + cantidadXP;
+    console.log(`Ganaste [${cantidadXP}] XP. Experiencia total: [${puntosExperiencia}] XP.`);
+}
+
+recibirDanio(20);
+ganarExperiencia(15);
+recibirDanio(50);
+ganarExperiencia(25);
+recibirDanio(40);
 mostrarEstadisticas();
